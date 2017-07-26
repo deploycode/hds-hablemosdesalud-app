4eb961b9a9b343a5b65c687dd1ddff3f1b3e2651
deploycode <gabriel@deploycode.co>
2017-07-26 09:37:04 -0500

    carousel updated

--- web/hds-layout/js/main.js
+++ web/hds-layout/js/main.js
@@ -34,23 +34,3 @@ $(document).ready(function(){
 			$('#menu').removeClass('in');
 	});
 });
-// carusel
-(function(){
-  $('.carousel-showmanymoveone .item').each(function(){
-    var itemToClone = $(this);
-
-    for (var i=1;i<4;i++) {
-      itemToClone = itemToClone.next();
-
-      // wrap around if at end of item collection
-      if (!itemToClone.length) {
-        itemToClone = $(this).siblings(':first');
-      }
-
-      // grab item, clone, add marker class, add to collection
-      itemToClone.children(':first-child').clone()
-        .addClass("cloneditem-"+(i))
-        .appendTo($(this));
-    }
-  });
-}());
